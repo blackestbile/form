2cfae4b874d1c7c2aa6d55210eb1927c41d052c3
blackestbile <soldego@riseup.net>
2024-07-16 08:45:28 -0300

    Create register.php

---
+++ register.php
@@ -0,0 +1,28 @@
+<?php
+echo $_POST['nome'];
+echo "<br>";
+
+echo $_POST['cor-fav'];
+echo "<br>";
+
+echo $_POST['idade'];
+echo "<br>";
+
+echo $_POST['tipo-sanguineo'];
+echo "<br>";
+
+echo $_POST['volume'];
+echo "<br>";
+
+echo $_POST['pesquisar'];
+echo "<br>";
+
+echo $_POST['cores'];
+echo "<br>";
+
+echo $_POST['hora_entrada'];
+echo "<br>";
+
+echo $_POST['telefone'];
+echo "<br>";
+?>
